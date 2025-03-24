a41d9b3236cb00c92a018bafbfa0315ccb6c64d1
BBDUYU <tmdwhddh@gmail.com>
2025-03-24 16:53:36 +0900

    tuto

---
+++ practice.py
@@ -0,0 +1,32 @@
+# 숫자형
+print(5) 
+print(-10)
+print(3.14)
+print(3+5) 
+print(2*8)
+print(3*(3+1))
+# 문자형
+print('풍선')
+print("나비")
+print("ㅋ"*9)
+
+# boolean
+print(5>10)
+print(2<3)
+print(True)
+print(False)
+print(not True)
+print(not(5>10))
+
+# 변수
+animal = '강아지'
+name = '연탄이'
+age = 4
+hobby = '산책'
+is_adult=age>=3
+
+print('우리집 '+animal+'의 이름은 '+name)
+print(name+'는'+str(age)+'살이고'+hobby+'를 아주 좋아한다')
+print(name+'는 어른일까 '+str(is_adult))
+
+# 주석
