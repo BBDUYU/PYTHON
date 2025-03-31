d19105e615647a648abc0f7b244a68b9f2e93308
BBDUYU <tmdwhddh@gmail.com>
2025-03-31 20:12:02 +0900

    승객매칭퀴즈

--- practice2.py
+++ practice2.py
@@ -19,3 +19,29 @@ print(students)
 students=['Iron man','Thor','Groot']
 students=[len(i) for i in students]
 print(students)
+#학생이름을 대문자로 변환
+students=['Iron man','Thor','Groot']
+students=[i.upper() for i in students]
+print(students)
+#------------------------------------------------------
+#quiz
+#50명의 승객과 매칭기회가 있을 때, 총 탑승 승객 수를 구하는 프로그램을 작성
+#조건1 : 승객별 운행 소여시간은 5~50분 사이의 난수
+#조건2 : 당신은 소요시간 5~15분 사이의 승객만 매칭
+#출력문 예제
+# [o] 1번째 손님 (소요시간 : 15분)
+# [ ] 2번째 손님 (소요시간 : 50분)
+# [o] 3번째 손님 (소요시간 : 5분)
+# ...
+# [ ] 50번째 손님 (소요시간 16분)
+# 총 탑승 승객 : 2명
+from random import *
+cnt=0
+for i in range(1,51): #1~50명
+    time=randrange(5,51) #5분~50분
+    if 5<=time <=15: #5분~15분 이내의 손님, 탑승 승객수 증가 
+        print('[o] {0}번째 손님 (소요시간 : {1}분)'.format(i,time))
+        cnt +=1
+    else: #매칭 실패한 경우
+        print('[ ] {0}번째 손님 (소요시간 : {1}분)'.format(i,time))
+print('총 탑승 승객 : {0}명'.format(cnt))
