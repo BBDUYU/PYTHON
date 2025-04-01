e33311dda3e6ccd8ac5906e1e91d2019cc01193b
BBDUYU <tmdwhddh@gmail.com>
2025-04-01 19:06:00 +0900

    가변인자

--- practice2.py
+++ practice2.py
@@ -45,3 +45,66 @@ for i in range(1,51): #1~50명
     else: #매칭 실패한 경우
         print('[ ] {0}번째 손님 (소요시간 : {1}분)'.format(i,time))
 print('총 탑승 승객 : {0}명'.format(cnt))
+#-----------------------------------------------------------
+#함수
+def open_account():
+    print('새로운 계좌가 생성되었습니다')
+
+open_account()
+#-------------------------------------------------------------------------------
+#전달값과 반환값
+def deposit(balance,money): #입금하려는 금액, 잔액
+    print('입금이 완료되었습니다. 잔액은{0} 원입니다'.format(balance+money))
+    return balance+money
+
+def withdraw(balance,money): #출금
+    if balance>=money: #잔액이 출금보다 많으면
+        print('출금이 완료되었습니다. 잔액은 {0}원입니다'.format(balance-money))
+        return balance-money
+    else:
+        print('출금이 완료되지 않았습니다. 잔액은 {0}원입니다'.format(balance))
+        return balance
+
+def withdraw_night(balance,money):
+    commission = 100 #수수료
+    return commission,balance-money-commission
+
+balance=0
+balance=deposit(balance,1000)
+#balance=withdraw(balance,500)
+commission,balance=withdraw_night(balance,500)
+print('수수료는 {0}원이며, 잔액은 {1}원입니다.'.format(commission,balance))
+#-------------------------------------------------------------------------------
+#기본값
+#def profile(name,age,main_lang):
+#    print('이름 : {0}\t나이 : {1}\t주 사용언어 : {2}'.format(name,age,main_lang))
+
+#profile('유재석',20,'파이썬')
+#profile('김태호',25,'자바')
+# 같은 학교, 같은 반, 같은 수업
+def profile(name,age=17,main_lang='파이썬'):
+    print('이름 : {0}\t나이 : {1}\t주 사용언어 : {2}'.format(name,age,main_lang))
+
+profile('유재석')
+profile('김태호')
+#---------------------------------------------------------------------------------
+#키워드 값
+def profile(name,age,main_lang):
+    print(name,age,main_lang)
+
+profile(name='유재석',main_lang='파이썬',age=20)
+profile(main_lang='자바',age=25,name='김태호')
+#---------------------------------------------------------------------------------
+#가변인자
+# def profile(name,age,lang1,lang2,lang3,lang4,lang5):
+#     print('이름 : {0}\t나이 : {1}\t'.format(name,age),end=' ')
+#     print(lang1,lang2,lang3,lang4,lang5)
+def profile(name,age,*language): # * 가변인자
+    print('이름 : {0}\t나이 : {1}\t'.format(name,age),end=' ')
+    for lang in language:
+        print(lang,end=' ')
+    print() #줄바꿈
+profile('유재석',20,'Python','Java','C','C++','C#','JavaScript')
+profile('김태호',25,'Kotlin','Swift')
+#------------------------------------------------------------------------------------
+#지역변수와 전역변수
