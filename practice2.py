f27cd72790a0e6fc6bbaba9017fe6cddd8da0ff0
BBDUYU <tmdwhddh@gmail.com>
2025-04-04 16:27:41 +0900

    with quiz

--- practice2.py
+++ practice2.py
@@ -164,3 +164,110 @@ for num in range(1,21):
 
 answer=input('아무 값이나 입력 : ')
 print('입력하신 값은 '+answer+'입니다')
+#---------------------------------------------------------------------------------
+#다양한 출력 포맷
+#빈 자리는 빈공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
+print('{0:>10}'.format(500))
+#양수일때 +표시 음수일때 -표시
+print('{0:>+10}'.format(500))
+print('{0:>+10}'.format(-500))
+#왼쪽 정렬, 빈칸을 _로 채움
+print('{0:_<10}'.format(500))
+#3자리마다 ,
+print('{0:,}'.format(1000000000000))
+#3자리마다 ,를 찍고 + - 부호 표시
+print('{0:+,}'.format(1000000000000))
+print('{0:+,}'.format(-1000000000000))
+#3자리마다 ,를 찍고 + - 부호 표시, 자릿수 확보, 빈자리 ^로 채우기
+print('{0:^<+30,}'.format(1000000000000000)) #빈칸, 정렬, 부호, 자릿수확보,3자리마다 콤마
+#소수점 출력
+print('{0:f}'.format(5/3))
+#소수점 특정 자리수까지만 표시 (소수점 3자리에서 반올림)
+print('{0:.2f}'.format(5/3))
+#---------------------------------------------------------------------------------
+#파일 입출력
+# score_file=open('score.txt','w',encoding='utf-8') #파일이름 작성 한글정보
+# print('수학 : 0',file=score_file)
+# print('영어 : 50',file=score_file)
+# score_file.close()
+
+#   파일 수정
+# score_file=open('score.txt','a',encoding='utf-8') #a = append 덮어쓰기
+# score_file.write('과학 : 80')
+# score_file.write('\n코딩 : 100') #write는 줄바꿈이 없음
+# score_file.close()
+
+#   읽는 방법 1
+#score_file=open('score.txt','r',encoding='utf-8') #파일 읽기
+#print(score_file.read())
+#score_file.close
+
+#   읽는 방법 2
+# score_file=open('score.txt','r',encoding='utf-8')
+# print(score_file.readline()) #줄 별로 읽기, 한줄 읽고 커서는 다음줄로 이동
+# print(score_file.readline())
+# print(score_file.readline())
+# print(score_file.readline())
+# score_file.close()
+
+#   읽는 방법 3
+# score_file=open('score.txt','r',encoding='utf-8')
+# while True: #무한루프
+#     line=score_file.readline() #score_file에서 한줄씩 불러온다
+#     if not line: #line이 없으면
+#         break #멈춰라
+#     print(line) #line이 있으면 print
+# score_file.close()
+
+#   읽는 방법 4
+score_file=open('score.txt','r',encoding='utf-8')
+lines=score_file.readlines() #모든 라인을 list형태로 저장
+for line in lines:  #line 변수를 지정해 lines의 내용을 하나씩 표시
+    print(line,end="")
+score_file.close()
+
+#---------------------------------------------------------------------------
+#pickle
+#데이터를 pickle을 이용해 파일에 저장후 파일에 저장된 내용을 load를 이용해 불러와 변수에 저장해 사용
+import pickle
+# profile_file=open('profile.pickle','wb') b = 바이너리 pickle은 작업방식에 b
+# profile={'이름' : '박명수','나이':30,'취미': ['축구','골프','코딩']}
+# print(profile)
+# pickle.dump(profile,profile_file) #profile에 있는 정보를 profile_file에 저장
+# profile_file.close()
+
+#profile_pickle에서 데이터를 불러오기
+profile_file=open('profile.pickle','rb')
+profile=pickle.load(profile_file) #file에 있는 정보를 profile에 불러오기
+print(profile)
+profile_file.close()
+#---------------------------------------------------------------------------
+#with
+import pickle
+with open('profile.pickle','rb') as profile_file: #profile.pickle파일을 열고 profile_file 변수에 파일정보를 저장
+    print(pickle.load(profile_file))
+    #with문을 탈출하며 open했던 파일을 자동으로 close
+
+# with open('study.txt','w',encoding='utf-8') as study_file:
+#     study_file.write('파이썬')
+
+with open('study.txt','r',encoding='utf-8') as study_file:
+    print(study_file.read())
+#-----------------------------------------------------------------------
+#퀴즈
+#회사에서 매주 1회 작성해야 하는 보고서가 있다
+#보고서는 항상 아래와 같은 형태로 출력되어야 한다
+
+#- X 주차 주간 보고 -
+#부서 : 
+#이름 :
+#업무 요약 :
+
+#1주차부터 50주차 까지의 보고서 파일을 만드는 프로그램을 작성하시오
+# 조건 : 파일명은 '1주차.txt', '2주차.txt', ... 와 같다
+for i in range(1,51):
+    with open(str(i)+'주차.txt','w',encoding='utf-8') as report_file:
+        report_file.write('- {0} 주차 주간 보고 -'.format(i))
+        report_file.write('\n부서 :')
+        report_file.write('\n이름 :')
+        report_file.write('\n업무 요약 : ')
