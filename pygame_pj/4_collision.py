8b7af174ac29802540f4adc5f64c6950ee19ff75
BBDUYU <tmdwhddh@gmail.com>
2025-04-30 15:05:16 +0900

    공과 무기 충돌

--- pygame_pj/4_collision.py
+++ pygame_pj/4_collision.py
@@ -75,6 +75,10 @@ balls.append({
     'init_spd_y' : ball_speed_y[0] # y 최초 속도
 })
 
+# 사라질 무기, 공 정보 저장변수
+weapon_to_remove = -1
+ball_to_remove = -1
+
 # 이벤트 루프
 running=True #게임이 진행중인가
 while running:
@@ -155,9 +159,37 @@ while running:
         ball_rect = ball_images[ball_img_idx].get_rect()
         ball_rect.left = ball_pos_x
         ball_rect.top = ball_pos_y
+
+        # 공과 캐릭터 충돌체크
         if character_rect.colliderect(ball_rect):
             running = False
             break
+        # 공과 무기 충돌처리
+        for weapon_idx, weapon_val in enumerate(weapons): # 리스트를 순회하며 몇 번째 인지 알려주며 해당하는 값을 가져옴 
+            weapon_pos_x = weapon_val[0]
+            weapon_pos_y = weapon_val[1]
+
+            # 무기 rect정보 업데이트
+            weapon_rect = weapon.get_rect()
+            weapon_rect.left = weapon_pos_x
+            weapon_rect.top = weapon_pos_y
+
+            if weapon_rect.colliderect(ball_rect):
+                weapon_to_remove = weapon_idx # 해당무기 없애기 위한 값 설정
+                ball_to_remove = ball_idx 
+                break
+    # 충돌된 공 없애기
+    if ball_to_remove > -1:
+        del balls[ball_to_remove] 
+        ball_to_remove = -1
+
+    if weapon_to_remove > -1:
+        del weapons[weapon_to_remove]
+        weapon_to_remove = -1
+
+
+
+        
 
 
 
