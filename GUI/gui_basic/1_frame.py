36becde9110cad53985d753de803f768c0c95c67
BBDUYU <tmdwhddh@gmail.com>
2025-05-02 19:12:01 +0900

    정리

---
+++ GUI/gui_basic/1_frame.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+root.title('PYTHON GUI')
+root.geometry('640x480') #가로세로
+#root.geometry('640x480+100+300) #가로 세로 x좌표 y좌표
+
+root.resizable(False,False) # x, y값 변경 불가
+
+root.mainloop()
