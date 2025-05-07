db1964e3de57a25065132ff45a360d7dcf5e0799
BBDUYU <tmdwhddh@gmail.com>
2025-05-07 22:50:24 +0900

    radio,check

--- GUI/gui_basic/10_menu.py
+++ GUI/gui_basic/10_menu.py
@@ -9,14 +9,33 @@ def create_new_file():
 
 menu = Menu(root)
 
+# File 메뉴
 menu_file = Menu(menu, tearoff=0)
 menu_file.add_command(label='New File',command=create_new_file)
 menu_file.add_command(label='New Window')
-menu_file.add_separator()
+menu_file.add_separator() # 구분선
 menu_file.add_command(label='Open File...')
+menu_file.add_separator() # 구분선
+menu_file.add_command(label='Save All',state='disabled') # 비활성화
+menu_file.add_separator()
+menu_file.add_command(label='Exit', command=root.quit)
 
 menu.add_cascade(label='File',menu=menu_file)
 
+# Edit 메뉴 (빈 값)
+menu.add_cascade(label='Edit')
+
+# Language 메뉴 추가 (라디오버튼)
+menu_lang=Menu(menu, tearoff=0)
+menu_lang.add_radiobutton(label='Python')
+menu_lang.add_radiobutton(label='Java')
+menu_lang.add_radiobutton(label='C++')
+menu.add_cascade(label='Language',menu=menu_lang)
+
+# View 메뉴 (체크박스)
+menu_view=Menu(menu,tearoff=0)
+menu_view.add_checkbutton(label='Show Minimap')
+menu.add_cascade(label='View', menu=menu_view)
 
 root.config(menu=menu)
 
