af256160ee52e068a5b578243c46992814add237
BBDUYU <tmdwhddh@gmail.com>
2025-05-29 18:40:09 +0900

    height, format

--- GUI/project/5_apply_options.py
+++ GUI/project/5_apply_options.py
@@ -92,6 +92,10 @@ def merge_image():
     max_width, total_height = max(widths), sum(heights)
 
     # 스케치북
+
+    if img_space > 0: # 이미지 간격 옵션 적용
+        total_height += (img_space * (len(images)-1))
+
     result_img = Image.new('RGB',(max_width,total_height),(255,255,255)) 
     y_offset=0 # y위치
     
@@ -100,14 +104,20 @@ def merge_image():
     #    y_offset += img.size[1] # height 값 만큼 더해줌
     
     for idx, img in enumerate(images):
+        # width가 원본이 아닐때에는 이미지 크기 조정
+        if img_width > -1:
+            img = img.resize(image_sizes[idx])
+
         result_img.paste(img,(0,y_offset))
-        y_offset += img.size[1]
+        y_offset += (img.size[1] + img_space) # height값 + 사용자가 지정한 간격
 
         progress = (idx+1) / len(images) * 100
         p_var.set(progress)
         progress_bar.update()
 
-    dest_path = os.path.join(txt_dest_path.get(),'nado_photo.jpg')
+    # 포맷 옵션 처리
+    file_name = 'nado_photo.'+img_format
+    dest_path = os.path.join(txt_dest_path.get(),file_name)
     result_img.save(dest_path)
     msgbox.showinfo('알림','작업이 완료되었습니다')
     
